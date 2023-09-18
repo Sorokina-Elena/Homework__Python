bfbeefdee9e68091e3389198c7d894139a19fd07
«Elena <e.sorokina761@gmail.com>
2023-09-18 12:40:22 +0400

    Добавлена домашнаяя работа

--- Seminar_08.py
+++ Seminar_08.py
@@ -1,3 +1,64 @@
+def read_csv(filename):
+    phone_book=[]
+    fields=['Фамилия', 'Имя', 'Телефон', 'Описание']
+    with open(filename,'r',encoding='utf-8') as phb:
+        for line in phb:
+            record = dict(zip(fields, line.split(',')))
+            phone_book.append(record)
+        return phone_book
+    
+
+def write_txt(filename, phone_book):
+    with open(filename,'w',encoding='utf-8') as phout:
+        for i in range(len(phone_book)):
+            s=''
+            for v in phone_book[i].values():
+                s+=v+','
+        phout.write(f'{s[:-1]}\n')   
+
+
+def find_by_lastname(phone_book, Last_name):
+    filtered = list(filter(lambda x: x['Фамилия'] == Last_name, phone_book))
+    return list(map(lambda x: x['Телефон'] + ' ' + x['Фамилия'] + ' ' + x['Имя'], filtered))
+
+
+def change_number(phone_book, Last_name, new_number):
+    for i in range(len(phone_book)):
+        if Last_name == phone_book[i]['Фамилия']:
+            key, value = 'Телефон', new_number
+            phone_book[i].update({key: value})
+    return phone_book
+
+
+def delete_by_lastname(phone_book, Last_name):
+    return list(filter(lambda x: x['Фамилия'] != Last_name, phone_book))
+
+
+def find_by_number(phone_book, number):
+    filtered = list(filter(lambda x: x['Телефон'] == number, phone_book))
+    return list(map(lambda x: x['Фамилия'] + ' ' + x['Имя'], filtered))
+
+
+def add_user(phone_book):
+    new_data = {}
+    Lastname = input('Фамилия: ')
+    key, value = 'Фамлия', Lastname
+    new_data.update({key: value})
+
+    firsname = input('Имя: ')
+    key, value = 'Имя', firsname
+    new_data.update({key: value})
+
+    phone = input('Телефон: ')
+    key, value = 'Телефон', phone
+    new_data.update({key: value})
+
+    description = input('Описание: ')
+    key, value = 'Описание', description
+    new_data.update({key: value})
+    phone_book.append(new_data)
+    return phone_book
+
 
 def show_menu():
     print('1. Распечатать справочник'
@@ -10,15 +71,6 @@ def show_menu():
     choice=int(input())
     return choice
 
-def read_csv(filename):
-    phone_book=[]
-    fields=['Фамилия', 'Имя', 'Телефон', 'Описание']
-    with open(filename,'r',encoding='utf-8') as phb:
-        for line in phb:
-            record = dict(zip(fields, line.split(',')))
-            phone_book.append(record)
-        return phone_book
-    
 
 def work_with_phonebook():
     choice=show_menu()
@@ -26,7 +78,7 @@ def work_with_phonebook():
     while (choice!=7):
         if choice==1:
             print(phone_book)
-        '''elif choice==2:
+        elif choice==2:
             last_name=input('lastname ')
             print(find_by_lastname(phone_book,last_name))
         elif choice==3:
@@ -42,17 +94,7 @@ def work_with_phonebook():
         elif choice==6:
             user_data=input('new data ')
             add_user(phone_book,user_data)
-            write_txt('phonebook.txt',phone_book)'''
+            write_txt('phonebook.txt',phone_book)
         choice=show_menu()
 
-
-def write_txt(filename, phone_book):
-    with open('phonebook.csv','w',encoding='utf-8') as phout:
-        for i in range(len(phone_book)):
-            s=''
-            for v in phone_book[i].values():
-                s+=v+','
-        phout.write(f'{s[:-1]}\n')
-
 work_with_phonebook()
-
